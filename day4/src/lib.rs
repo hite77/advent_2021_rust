e909f7de8af349300baeb9aa47f2b0b78c1e90ca
Jason Hite <hite77@gmail.com>
2022-01-09 16:46:53 -0500

    Day 4 part 2

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -12,6 +12,37 @@ impl Bingo {
 		Bingo {unmarked: 0, lastcalled: 0}
 	}
 
+    pub fn bingo_on_board(&self, board: Vec<Vec<i32>>) -> bool {
+        let mut result: bool = false;
+        // horizontal bingo
+        for row in 0..5 {
+            result = true;
+            for item in 0..5 {
+                if board[row][item] != -1 {
+                    result = false;
+                    break;
+                }
+            }
+            if result {
+                return true;
+            }
+        }
+        // vertical bingo
+        for item in 0..5 {
+            result = true;
+            for row in 0..5 {
+                if board[row][item] != -1 {
+                    result = false;
+                    break;
+                }
+            }
+            if result {
+                return true;
+            }
+        }
+        result
+    }
+
     pub fn total_solution(&self, board: Vec<Vec<i32>>) -> i32 {
         let mut result : i32 = 0;
         for row in 0..5 {
@@ -43,33 +74,9 @@ impl Bingo {
         let mut solution_board: Vec<Vec<i32>> = vec![];
 
         for board in 0..boards.len() {
-            // horizontal bingo
-            for row in 0..5 {
+            if self.bingo_on_board(boards[board].clone()) {
                 result = true;
-                for item in 0..5 {
-                    if boards[board][row][item] != -1 {
-                        result = false;
-                        break;
-                    }
-                }
-                if result {
-                    solution_board = boards[board].clone();
-                    return (result, solution_board)
-                }
-            }
-            // vertical bingo
-            for item in 0..5 {
-                result = true;
-                for row in 0..5 {
-                    if boards[board][row][item] != -1 {
-                        result = false;
-                        break;
-                    }
-                }
-                if result {
-                    solution_board = boards[board].clone();
-                    return (result, solution_board)
-                }
+                solution_board = boards[board].clone();
             }
         }
         (result, solution_board)
@@ -119,12 +126,68 @@ impl Bingo {
 		self.lastcalled * self.unmarked
 	}
 
+    pub fn part_two(&mut self, contents: &str) -> i32 {
+        let mut boards: Vec<Vec<Vec<i32>>> = vec![];
+        let mut board: Vec<Vec<i32>> = vec![];
+        let mut count: i32 = 0;
+
+        let mut called_numbers : Vec<i32> = vec![];
+
+        for line in contents.lines() {
+            if line.contains(",") {
+                for item in line.split(",") {
+                    called_numbers.push(item.parse().unwrap());
+                }
+            }
+            else if line != "" {
+                if count == 0 {
+                    board = vec![];
+                    count += 1;
+                }    
+                let mut row: Vec<i32> = vec![];
+                for item in line.split(" ") {
+                    if item != "" {
+                        row.push(item.parse().unwrap());
+                    }
+                }
+                count += 1;
+                board.push(row);
+                if count > 5 {
+                    boards.push(board.clone());
+                    count = 0;
+                }
+            }
+       }
+        for item in called_numbers {
+           boards = self.mark_off_called(boards, item); 
+
+           if boards.len() > 1 {
+               let mut boards_without_bingo: Vec<Vec<Vec<i32>>> = vec![];
+               for index_of_boards in 0..boards.len() {
+                    if false == self.bingo_on_board(boards[index_of_boards].clone()) {
+                        boards_without_bingo.push(boards[index_of_boards].clone());
+                    }
+               }
+               boards = boards_without_bingo;            
+           }
+
+           if boards.len() == 1 {
+                let (bingo_happened, solution_board) = self.detect_bingo(boards.clone());
+                if bingo_happened {
+                    self.lastcalled = item;
+                    self.unmarked = self.total_solution(solution_board.clone());
+                    return self.lastcalled * self.unmarked;
+                }
+            }
+        }
+        self.lastcalled * self.unmarked
+    }
 }
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string("input")?;
     println!("day4 part1 answer: {}", Bingo::new().part_one(&contents));
-    //println!("day3 part2 answer: {}", BinaryPart2::new().parse(&contents));
+    println!("day4 part2 answer: {}", Bingo::new().part_two(&contents));
     Ok(())
 }
 
@@ -251,5 +314,34 @@ mod tests {
         assert_eq!(188, bingo.unmarked);
         assert_eq!(24, bingo.lastcalled); 
         assert_eq!(4512, answer);
+    }
+
+    #[test]
+    fn first_example_part_two() {
+        let contents = "\
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
+        let mut bingo = Bingo::new();
+        let answer = bingo.part_two(&contents);
+        assert_eq!(148, bingo.unmarked);
+        assert_eq!(13, bingo.lastcalled); 
+        assert_eq!(1924, answer);
     }
 }
